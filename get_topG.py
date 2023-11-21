caa7a274c349da1464c5f421858a8ce20086eaf1
jaffer <jafferrazavi@gmail.com>
2023-11-20 21:32:48 -0500

    ALLAHUAKBAR

---
+++ get_topG.py
@@ -0,0 +1,46 @@
+import requests as re
+import csv
+import PyPDF2 as pdf
+from io import BytesIO
+
+
+# Fetch the PDF from the URL: 
+# Use the requests library to get the
+# PDF from the provided URL.
+# 
+# Read the PDF Content: 
+# Read the content of the PDF in memory 
+# using a PDF processing library.
+# 
+# Extract the Text: 
+# Extract text from each page of the PDF.
+# 
+# Process the Text: 
+# Depending on your needs, process the text 
+# (like splitting into sections, cleaning, etc.).
+#
+#  Write to CSV: 
+# Use Python's csv library to write the processed text into a CSV file.
+
+url = "https://www.sistani.org/files-new/book-pdf/english-islamic-laws-2023.pdf"
+
+soup = re.get(url) #we soupin this bitch
+fatwa_content = BytesIO(soup.content)
+
+#scan the halalness
+halal_reader = pdf.PdfReader(fatwa_content)
+
+#now that the blessings have been extracted,
+# lets throw it into satans database
+
+with open('topG.csv', 'w', newline='', encoding='utf-8') as topG_file:
+    write_fatwa = csv.writer(topG_file)
+
+    for page_number in range(len(halal_reader.pages)):
+        page = halal_reader.pages[page_number]
+        text = page.extract_text()
+        write_fatwa.writerow([text])
+
+fatwa_content.close()
+
+
