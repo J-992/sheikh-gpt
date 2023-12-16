b160708181cd8dbf8e1ba618d4de9f131d8d864b
jaffer <jafferrazavi@gmail.com>
2023-12-16 15:01:26 -0500

    Update get_topG.py

--- get_topG.py
+++ get_topG.py
@@ -4,38 +4,38 @@ import PyPDF2 as pdf
 from io import BytesIO
 
 
-# Fetch the PDF from the URL: 
-# Use the requests library to get the
-# PDF from the provided URL.
-# 
-# Read the PDF Content: 
-# Read the content of the PDF in memory 
-# using a PDF processing library.
-# 
-# Extract the Text: 
-# Extract text from each page of the PDF.
-# 
+
+# NOTE: Whats left to do:
 # Process the Text: 
 # Depending on your needs, process the text 
 # (like splitting into sections, cleaning, etc.).
 #
-#  Write to CSV: 
-# Use Python's csv library to write the processed text into a CSV file.
 
+
+# Fetch the PDF from the URL: 
+# Use the requests library to get the
+# PDF from the provided URL.
 url = "https://www.sistani.org/files-new/book-pdf/english-islamic-laws-2023.pdf"
 
 soup = re.get(url) #we soupin this bitch
 fatwa_content = BytesIO(soup.content)
 
-#scan the halalness
-halal_reader = pdf.PdfReader(fatwa_content)
+
+# Read the PDF Content: 
+# Read the content of the PDF in memory 
+# using a PDF processing library such as PyPDF
+halal_reader = pdf.PdfReader(fatwa_content) #scan the halalness
 
 #now that the blessings have been extracted,
 # lets throw it into satans database
 
+#  Write to CSV: 
+# Use Python's csv library to write the processed text into a CSV file.
 with open('topG.csv', 'w', newline='', encoding='utf-8') as topG_file:
     write_fatwa = csv.writer(topG_file)
 
+    # Extract the Text: 
+    # Extract text from each page of the PDF.
     for page_number in range(len(halal_reader.pages)):
         page = halal_reader.pages[page_number]
         text = page.extract_text()
